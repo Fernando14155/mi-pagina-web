2d9847257575b8fcb51e424c0327e1e9b2da0ced
Fernando14155 <josefernandochavezramirez3@gmail.com>
2025-05-17 13:27:23 -0600

    solucion

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const btnMenu = document.getElementById("btnMenu");
-const sidebar = document.getElementById("sidebar");
 const btnCerrar = document.getElementById("btnCerrar");
 
 btnMenu.addEventListener("click", () => {
-  sidebar.classList.add("abierto");
+  document.body.classList.add("sidebar-abierto");
 });
 
 btnCerrar.addEventListener("click", () => {
-  sidebar.classList.remove("abierto");
+  document.body.classList.remove("sidebar-abierto");
 });
