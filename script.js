1719768988a34c6b7713febdbce2898a93b59134
Fernando14155 <josefernandochavezramirez3@gmail.com>
2025-05-17 21:51:42 -0600

    agregar busqueda

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ document.getElementById("btnCerrar").addEventListener("click", () => {
   document.body.classList.remove("sidebar-abierto");
 });
 
+// Filtro de búsqueda
+document.getElementById("busqueda").addEventListener("keyup", function () {
+  const filtro = this.value.toLowerCase();
+  const items = document.querySelectorAll("#menuItems li");
+
+  items.forEach((item) => {
+    const texto = item.textContent.toLowerCase();
+    item.style.display = texto.includes(filtro) ? "block" : "none";
+  });
+});
+
 // Cierra si haces clic en cualquier enlace
 document.querySelectorAll(".sidebar a").forEach(link => {
   link.addEventListener("click", () => {
