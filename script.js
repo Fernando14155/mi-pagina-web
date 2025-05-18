9d491f52df2f8c1c26d404e096373ab7634cc085
Fernando14155 <josefernandochavezramirez3@gmail.com>
2025-05-17 22:00:38 -0600

    inicioNuevo

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+// Abrir menú
 document.getElementById("btnMenu").addEventListener("click", () => {
   document.body.classList.add("sidebar-abierto");
 });
 
+// Cerrar menú
 document.getElementById("btnCerrar").addEventListener("click", () => {
   document.body.classList.remove("sidebar-abierto");
 });
 
-// Cierra si haces clic en cualquier enlace
+// Cerrar menú si se hace clic en cualquier enlace
 document.querySelectorAll(".sidebar a").forEach(link => {
   link.addEventListener("click", () => {
     document.body.classList.remove("sidebar-abierto");
