6821e17dd31cff7deae4ae2479213e4563308ba2
Fernando14155 <josefernandochavezramirez3@gmail.com>
2025-05-17 22:12:39 -0600

    nuevomodel

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+// Archivo JS básico para futuras funcionalidades
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Página cargada correctamente. Listo para futuras interacciones.");
+});
