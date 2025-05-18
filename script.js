2d5cb5625f37055ef83a7f35444e5ea042ee5ed4
Fernando14155 <josefernandochavezramirez3@gmail.com>
2025-05-17 21:44:16 -0600

    Nuevo inicio v1.2

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
 document.getElementById("btnMenu").addEventListener("click", () => {
-    document.body.classList.add("sidebar-abierto");
+  document.body.classList.add("sidebar-abierto");
 });
 
 document.getElementById("btnCerrar").addEventListener("click", () => {
+  document.body.classList.remove("sidebar-abierto");
+});
+
+// Cierra si haces clic en cualquier enlace
+document.querySelectorAll(".sidebar a").forEach(link => {
+  link.addEventListener("click", () => {
     document.body.classList.remove("sidebar-abierto");
-});
-
-// Cerrar al hacer clic en cualquier enlace del sidebar
-document.querySelectorAll(".sidebar ul li a").forEach(link => {
-    link.addEventListener("click", () => {
-        document.body.classList.remove("sidebar-abierto");
-    });
-});
-
-// Cerrar al hacer clic fuera del sidebar (excepto en el botón de menú)
-document.addEventListener("click", (e) => {
-    const sidebar = document.getElementById("sidebar");
-    const btnMenu = document.getElementById("btnMenu");
-
-    const clickDentroSidebar = sidebar.contains(e.target);
-    const clickBotonMenu = btnMenu.contains(e.target);
-
-    if (!clickDentroSidebar && !clickBotonMenu) {
-        document.body.classList.remove("sidebar-abierto");
-    }
+  });
 });
