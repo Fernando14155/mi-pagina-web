0748dbd0165f355c6eb4d753d26d456532194059
Fernando14155 <josefernandochavezramirez3@gmail.com>
2025-05-17 21:52:38 -0600

    borrar busqueda

--- script.js
+++ script.js
@@ -6,16 +6,7 @@ document.getElementById("btnCerrar").addEventListener("click", () => {
   document.body.classList.remove("sidebar-abierto");
 });
 
-// Filtro de búsqueda
-document.getElementById("busqueda").addEventListener("keyup", function () {
-  const filtro = this.value.toLowerCase();
-  const items = document.querySelectorAll("#menuItems li");
 
-  items.forEach((item) => {
-    const texto = item.textContent.toLowerCase();
-    item.style.display = texto.includes(filtro) ? "block" : "none";
-  });
-});
 
 // Cierra si haces clic en cualquier enlace
 document.querySelectorAll(".sidebar a").forEach(link => {
