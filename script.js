f1f474d772b7f357f2b6f2301187839524927036
Fernando14155 <josefernandochavezramirez3@gmail.com>
2025-05-17 21:53:58 -0600

    borrar busqueda

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ document.getElementById("btnCerrar").addEventListener("click", () => {
   document.body.classList.remove("sidebar-abierto");
 });
 
-
-
 // Cierra si haces clic en cualquier enlace
 document.querySelectorAll(".sidebar a").forEach(link => {
   link.addEventListener("click", () => {
